2140b2a3c3a88b44ac850a9a51418fe4d8740e8d
ankurgajurel <ankurgajurel02@gmail.com>
2022-12-25 03:54:26 +0545

    commands added and updated: news, price and top

--- README.md
+++ README.md
@@ -1,7 +1,52 @@
-for help, use the command:
-python3 app.py --help
+# Change into the git directory
+```cd nepse-api```
 
-USE METHODS
-python3 app.py company-profile adbl
 
-python3 app.py price adbl
+# Install the required dependencies
+## For Windows,
+```pip install -r requirements.txt```
+## For Linux / UNIX,
+```pip3 install -r requirements.txt```
+
+Or, if you don't want the hassle, you can also run the req.py file. This will install the dependencies for you.
+```python3 req.py```
+
+For help, use the command:
+```python3 app.py --help```
+
+# Using the app
+```python3 app.py company-profile adbl```
+
+This command will print the company profile of the scrip "ADBL". The company profile at the moment only includes the full name of the company and the type of the scrip, if it is a stock or a mutual fund or a debenture, etc
+
+```python3 app.py price adbl```
+
+This command will print the latest price of the scrip "ADBL". The price will be in NRS and is not tested if it will update live whilst the NEPSE Market is open. 
+
+You can also extract the latest breaking news related to NEPSE. There might not be many news beacuse this command will extract only important ones. 
+
+```python3 app.py news```
+
+If you want to extract certain amount of news,
+
+```python3 app.py news --n 10```
+
+You can also view the top gainers, loosers and the top turnover using the "top" command. The top command has one required argument. The argument might either be "gainer" or "looser" or "turnover". It will print a tuple of the symbol and the value of the property. 
+
+```python3 app.py top gainer```
+
+This will print the top 5 gainers.
+
+```python3 app.py top looser```
+
+```python3 app.py top turnover```
+
+If you want to print a certain number of top scrips, you can use the option --n.
+
+For example, 
+
+```python3 app.py top gainer --n 10```
+
+This command will print the top 10 gainers of the latest trading day.
+
+Remember that the max value for --n is 10. So, app will throw an exception of list out of range
--- app.py
+++ app.py
@@ -1,30 +1,88 @@
-import typer
-import requests
 import json
+import requests
+import typer
 
-def send_req(scrip: str) -> dict:
-    BASE = "https://www.nepsealpha.com/trading-menu/top-stocks/"
-    return json.loads(requests.get(BASE + scrip.upper()).text)
+def send_req_basic(scrip: str) -> dict:
+    """This is the basic send request function that will get the basic info of a company"""
+    base_url = "https://www.nepsealpha.com/trading-menu/top-stocks/"
+    return json.loads(requests.get(base_url + scrip.upper()).text)
+
+def send_req_news() -> dict:
+    """
+        This send req will requests data for latest day's breaking news. 
+        The news might be less in number because they are special ones.
+    """
+
+    base_url = "https://www.nepsealpha.com/api/smx9841/get_breaking_news"
+    return json.loads(requests.get(base_url).text)['breaking']
+    
+def send_req_price(scrip: str) -> float:
+    """
+        This function will request raw data for the price of the company
+    """
+    base_url = "https://www.nepsealpha.com/trading/1/history?symbol=" + scrip.upper() + "&resolution=1D&from=1638921600&to=99999999999"
+    return float(json.loads(requests.get(base_url).text)['c'][-1])
+
+def send_req_top_stocks(rank: int, field: str) -> tuple:
+    """
+        This function will request raw data about top gainer or looser or turnover.
+    """
+    base_url = "https://www.nepsealpha.com/trading-menu/top-stocks/"
+    return json.loads(requests.get(base_url).text)['topStocks'][rank][field][0:2]
 
 app = typer.Typer()
 
 @app.command()
 def company_profile(scrip):
+    """
+        This command will print the company profile of the argument symbol.
+        There are no extra options for this command
+    """
     try:
-        info = send_req(scrip)['info']
+        info = send_req_basic(scrip)['info']
         for i in info:
             print(f'{i} --> {info[i]}')
-
-    except Exception as e:
-        print(f"could not fetch info because {e}")
+    except Exception as get_exception:
+        print(f"could not fetch info because {get_exception}")
 
 @app.command()
-def price(scrip: str, command='today_price'):
+def price(scrip: str) -> None:
+    """
+        This command will print the price of the argument symbol.
+        There are no extra options for this command.
+    """
     try:
-        price_info = send_req(scrip)['price']
-        typer.echo(f"{command} = {price_info[command]}")
-    except Exception as e:
-        ...
+        typer.echo(f"Today's price of {scrip.upper()} is {send_req_price(scrip)}")
+    except Exception as what_exception:
+        print(f"Could not process price because {what_exception}")
+
+@app.command()
+def news(n: int = 3):
+    """
+        This command will print the top 3 news of the argument symbol.
+        There is one extra optional "option" which the number of news.
+    """
+    news_list = list(send_req_news())
+    my_str = ""
+    try:
+        for news_ in news_list[0:n]:
+            my_str = my_str + "\n" + news_['title']
+        typer.echo(my_str)
+    except Exception as what_exception:
+        print(f"Could not process news because {what_exception}")
+
+@app.command()
+def top(field:str, n=5):
+    """
+        This command will print the top 5 stock of the argument symbol.
+        There is one optional "option" for this commmand which is the total number of scrips to be prints.
+    """
+    for i in range(int(n)):
+        try:
+            typer.echo(f"{send_req_top_stocks(i, field)}")
+        except Exception as what_exception:
+            print(f"Could not process because {what_exception}")
+
 
 if __name__ == "__main__":
     app()
